007a970c72f7d924a429e67ecc60c7191b458ab7
Augustus Ostow <ostowster@gmail.com>
2025-10-07 07:00:10 -0400

    Call transit crate in api main.rs

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -2,5 +2,8 @@ use anyhow::Result;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
+    let client = transit::TransitClient::from_env()?;
+    let response = client.stop_departures("MTAS:18774").await?;
+    println!("{:#?}", response);
     Ok(())
 }
