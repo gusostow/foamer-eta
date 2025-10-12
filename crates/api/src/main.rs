aa5c0bc8e227d3c2f66fc308819e39cae5a378d2
Augustus Ostow <ostowster@gmail.com>
2025-10-12 19:20:17 -0400

    Add svc integration test

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -5,7 +5,7 @@ async fn main() -> Result<()> {
     let app = api::svc::create_router()?;
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
-    println!("Server listening on {}", listener.local_addr()?);
+    eprintln!("Server listening on {}", listener.local_addr()?);
 
     axum::serve(listener, app).await?;
 
