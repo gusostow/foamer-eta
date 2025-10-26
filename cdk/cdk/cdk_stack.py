b5cbbad1a0c8e93786c2d875b9b1bbe87f7d8bb9
Augustus Ostow <ostowster@gmail.com>
2025-10-26 13:57:56 -0400

    Create dev and prod stacks

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -1,19 +1,20 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    RemovalPolicy,
+    aws_s3 as s3,
 )
 from constructs import Construct
 
 class CdkStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, env_name: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Test S3 bucket
+        bucket = s3.Bucket(
+            self, "FoamerTestBucket",
+            bucket_name=f"foamer-{env_name}-bucket",
+            versioned=False,
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+        )
