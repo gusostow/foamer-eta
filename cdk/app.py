deff538c5df65b7b0c52157b6214bed3a03639b4
Augustus Ostow <ostowster@gmail.com>
2025-10-29 12:25:49 -0400

    Create untested CI deployment workflow

--- cdk/app.py
+++ cdk/app.py
@@ -4,6 +4,7 @@ import os
 import aws_cdk as cdk
 
 from cdk.cdk_stack import CdkStack
+from cdk.ci_stack import CIStack
 
 
 app = cdk.App()
@@ -13,6 +14,10 @@ env = cdk.Environment(
     account=os.environ.get("CDK_DEFAULT_ACCOUNT"),
     region=os.environ.get("CDK_DEFAULT_REGION", "us-east-1")
 )
+
+# CI/CD identity stack (for GitHub Actions)
+# Bootstrapped by running locally
+CIStack(app, "FoamerCI", github_repo="gusostow/foamer-eta", env=env)
 
 # Development stack
 CdkStack(app, "FoamerDev", env_name="dev", env=env)
